b9265909784948b59c46a6f6bde4ad75333f29c4
Vishnu Ravi <vishnu.ravi@gmail.com>
2023-11-19 13:35:16 -0500

    Fix linter errors

--- __tests__/mock.test.js
+++ __tests__/mock.test.js
@@ -1,5 +1,15 @@
+/*
+
+This source file is part of the Stanford Spezi Template Web Dashboard open-source project
+
+SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
+
+SPDX-License-Identifier: MIT
+   
+*/
+
 describe('Mock Test Suite', () => {
-    it('should pass a mock test', () => {
-        expect(true).toBeTruthy();
-    });
+  it('should pass a mock test', () => {
+    expect(true).toBeTruthy();
+  });
 });
