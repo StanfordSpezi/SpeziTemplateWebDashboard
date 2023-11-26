885be09e052533f6fb4a9af5997aba8fe3a7ccf6
Caitlin Kunchur <crkunchur@gmail.com>
2023-11-26 14:00:16 -0800

    license info

--- src/app/components/Auth/StanfordSSO.tsx
+++ src/app/components/Auth/StanfordSSO.tsx
@@ -1,4 +1,13 @@
 
+/*
+
+This source file is part of the Stanford Spezi Template Web Dashboard open-source project
+
+SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
+
+SPDX-License-Identifier: MIT
+   
+*/
 
 import React, { useState, useEffect } from 'react';
 import { getAuth, signInWithPopup, OAuthProvider } from "firebase/auth";
