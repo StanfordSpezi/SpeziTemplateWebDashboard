f817f5dcdb511e1d131f0240674239c66b663950
Vishnu Ravi <vishnur@stanford.edu>
2024-01-28 16:01:48 -0500

    Remove unused packages

--- packages/cloud-storage-connector/dist/firebase.d.ts
+++
@@ -1,5 +0,0 @@
-declare const app: import("@firebase/app").FirebaseApp;
-export declare const db: import("@firebase/firestore").Firestore;
-export declare const auth: import("@firebase/auth").Auth;
-export default app;
-//# sourceMappingURL=firebase.d.ts.map
--- packages/smart-on-fhir-connector/dist/Launcher.d.ts
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-export default function Launcher(): React.JSX.Element;
-//# sourceMappingURL=Launcher.d.ts.map
